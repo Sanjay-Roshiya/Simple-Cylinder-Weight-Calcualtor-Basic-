069ec45b2b474d7e3d2639f2cbe1c66e6b556732
Sanjay-Roshiya <sanjayrosiya2002@gmail.com>
2026-03-16 00:00:21 +0530

    Add files via upload

---
+++ weight_calculator.py
@@ -0,0 +1,6 @@
+a = int(input("Rod Diameter: "))
+b = int(input("Length of Rod :"))
+c = float(input("density gm/cm^3 :"))
+volume = (22/7)*((a/2)**2)*b
+print("Volume = ", volume, "mm^3")
+print("Weight = ", (volume*(c/10**6)/1000), "Grams", "or",(volume*(c/10**6)), "Kgs")
